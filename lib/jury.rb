3a6513f9438b03cfaf502bd4ed66965dad54d756
mjsmith3406 <mjsmith3406@outlook.com>
2016-07-24 20:24:27 -0500

    jury is finally done and working

--- lib/jury.rb
+++ lib/jury.rb
@@ -1,12 +1,41 @@
 class Jury
- attr_reader :members
+ attr_accessor :members
 
   def initialize
-    @members = members
-  end
-  def add_member(members)
+    @members = []
+    @finalists = Hash.new
 
   end
+  def add_member(member)
+    @members << member
+  end
 
+  def cast_votes(finalists)
+     finalists.each do |finalist|
+       @finalists[finalist] = 0
+     end
 
+    @members.each do |member|
+      puts "#{member.name} has voted for #{finalists.sample}"
+      @finalists[finalists.sample] += 1
+    end
+    return @finalists
+  end
+
+  def report_votes(final_votes)
+    final_votes.each do |finalist, score|
+      puts "#{finalist} : #{score}"
+    end
+  end
+
+  def report_votes(final_votes)
+    final_votes.each do |finalist, score|
+      puts "#{finalist} : #{score}"
+    end
+  end
+
+  def announce_winner(final_votes)
+    final_votes.max_by { |_, vote| vote }.first
+
+  end
 end
