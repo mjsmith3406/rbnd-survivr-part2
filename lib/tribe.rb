f2397bb24c8a71d52b0492302dbd9b609ea28ad4
mjsmith3406 <mjsmith3406@outlook.com>
2016-07-25 21:54:51 -0500

    tribe has fails. need fixed.

--- lib/tribe.rb
+++ lib/tribe.rb
@@ -1,8 +1,23 @@
 class Tribe
-  attr_accessor :name, :members
+  attr_accessor :members
 
-  def initialize(name)
-    @name = name
+  def initialize(tribe)
+    @name = tribe[:name]
+    @members = tribe[:members]
+    puts "Tribe #{@name} was created"
   end
 
+  def name
+    puts "#{@name}"
+  end
+
+    def tribal_council(options={})
+      selected_member = @members.sample
+      while selected_member == options[:immune]
+        selected_member = @members.sample
+      end
+      puts "#{selected_member} was voted off"
+      @members.delete(selected_member)
+      puts selected_member
+    end
 end
