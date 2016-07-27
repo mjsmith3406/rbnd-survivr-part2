6d975329c0db7d437b05fabd997b1cb8c6b80c08
mjsmith3406 <mjsmith3406@outlook.com>
2016-07-27 15:55:37 -0500

    good start to figuring out problem.

--- lib/game.rb
+++ lib/game.rb
@@ -1,13 +1,14 @@
 class Game
-  attr_accessor :tribes, :name, :members
+  attr_accessor :tribes, :name, :members, :tribe
 
   def initialize(name, members)
     @name = name
     @members = members
     @tribes = []
+    @tribe = tribe
   end
 
-  def add_tribe
-    
+  def add_tribe(tribe)
+    @tribes << @tribe
   end
 end
